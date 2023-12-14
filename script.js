5ce05b0079fb8864c6c14dc720e807f275aa9521
1LightHouse1 <vini_nascimento1@outlook.com>
2023-12-14 16:07:14 -0300

    adicionando o fechamento ao formulário

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ document.getElementById('show-form-btn').addEventListener('click', function() {
     document.getElementById('form-container').classList.add('d-block');
     document.getElementById('overlay').style.display = 'block';
 });
+
+document.getElementById('close-form-btn').addEventListener('click', function(){
+    document.getElementById('form-container').classList.add('none');
+    document.getElementById('overlay').style.display = 'none';
+})

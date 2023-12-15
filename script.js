8531faea1e080125ca1a825568bba9ceabcb37e0
LeandroClayton <leandroclaytonp@gmail.com>
2023-12-15 00:03:24 -0300

    Fiz o bgl de criar cards e salvar no localstorage

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const url = 'https://referential.p.rapidapi.com/v1/country?fields=currency%2Ccur
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'chave',
+		'X-RapidAPI-Key': 'a6f3d1fffcmshfe00ea9395d581fp13ba85jsncf8248095449',
 		'X-RapidAPI-Host': 'referential.p.rapidapi.com'
 	}
 };
 
 let result2 = "";
 const selectElement = document.getElementById('local');
+let divContainer = document.querySelector(".cards");
+
+var listaViagens = [];
+let form = document.forms["cadastroDeViagem"];
 
 async function fetchData() {
     try {
@@ -46,4 +50,54 @@ document.getElementById('close-form-btn').addEventListener('click', function(){
     document.getElementById('form-container').classList.add('none');
     document.getElementById('overlay').style.display = 'none';
 })
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (this.local.value === "Selecione um país") {
+        window.alert("SELECIONE ALGUM PAÍS");
+    }
+    let viagem = {
+        "id": listaViagens.length + 1,
+        "Local": this.local.value,
+        "Cidade": this.cidade.value,
+        "Descricao": this.descricao.value,
+        "nota": this.nota.value,
+    };
+    listaViagens.push(viagem)
+    
+    localStorage.setItem('viagens',JSON.stringify(listaViagens));
+});
+
+window.addEventListener('load', () =>{
+    listaViagens = JSON.parse(localStorage.getItem('viagens'));
+
+    if(listaViagens!= null){
+        criarCards(listaViagens);
+    }
+})
+
+function criarCards(listaViagensJSON) {
+    let container = document.querySelector(".cards");
+    listaViagensJSON.forEach(viagem => {
+        let card = `
+        <div class="card-body">
+            <h5 class="card-title">${viagem.Local}</h5>
+            <p class="card-text">${viagem.Cidade}</p>
+            <p class="card-text">${viagem.Descricao}</p>
+            <p class="card-text">${viagem.nota}</p>
+        </div>
+        <div class="card-footer">
+            <a href="#" class="btn btn-primary">Editar</a>
+            <a href="#" class="btn btn-danger">Excluir</a>
+        </div>
+        `;
+        divContainer.innerHTML += card;
+    });
+}
+
+function deletarCard(){
+    
+}
+
+
 
